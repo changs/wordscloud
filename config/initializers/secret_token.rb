036f634acb5c16dd16c96fdca34e87b056841f1f
Bartosz Pranczke <bartosz.pranczke@gmail.com>
2011-07-26 12:51:48 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wordscloud::Application.config.secret_token = '39b8e09b3cc4fcb63a355eefb4eb5976971959fcc7fc688deb90e63e60f21614e427e1adad3f70dc92f8acf8bdd99e59bf4e4b6de6b6a78cd1a52ff4a968e8b2'
