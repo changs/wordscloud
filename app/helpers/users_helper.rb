355922e88d6f788cf09e6f7fc91f4bfc6cfec1e3
Bartosz Pranczke <pranczke@gmail.com>
2011-08-05 10:43:07 +0200

    add gravatar helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def avatar_for(user, size = 48)
+    default_url = "#{root_url}assets/guest.jpg"
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    image_tag "http://gravatar.com/avatar/#{gravatar_id}.png
+                ?s=#{size}&d=#{CGI.escape(default_url)}",
+                size: "#{size}x#{size}"
+  end
 end
