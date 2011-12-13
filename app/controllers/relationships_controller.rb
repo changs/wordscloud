31dcc4d4d79d4409ea4dc6b11844d4b4ffa069c1
Bartosz Pranczke <pranczke@gmail.com>
2011-12-13 12:32:24 +0100

    Fix relationship controller

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,13 +4,11 @@ class RelationshipsController < ApplicationController
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
     redirect_to(:back)
-    end
   end
 
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
-      redirect_to(:back)
-    end
+    redirect_to(:back)
   end
 end
