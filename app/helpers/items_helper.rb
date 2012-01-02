e1c62b044e8412722f490aacc7a4beec9df85519
Bartosz Pranczke <pranczke@gmail.com>
2012-01-02 13:53:21 +0100

    Changed homepage

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -6,4 +6,7 @@ module ItemsHelper
   def how_many_items
     current_user.items.count
   end
+  def remembered_items(user)
+    current_user.items.where("review_at > ?", Time.now+30.days).count
+  end
 end
